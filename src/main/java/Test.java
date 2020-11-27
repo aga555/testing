e849a88b3b11a7b04dc39de84c24c65aaf4be2c4
akrawczyk <agammkrawczyk@gmail.com>
2020-11-27 18:00:21 +0100

    testing method some  use cases difrent  numbers

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,9 @@
 public class Test {
     public static void main(String[] args) {
    System.out.println(areEqual(4,2+2));
+   System.out.println(areEqual(5,2+3));
+   System.out.println(areEqual(8,5+3));
+   System.out.println(areEqual(7,8-1));
     }
     private  static boolean areEqual(int expected, int actual){
         return actual==expected;
