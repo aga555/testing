ef16021a5c956a4a5610bea87fabba0f7a0952f0
akrawczyk <agammkrawczyk@gmail.com>
2020-11-27 17:58:39 +0100

    testing method with  without temprorary virables

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-       int expected =4;
-       int actual= 2+2;
-       boolean result= areEqual(expected,actual);
+   System.out.println(areEqual(4,2+2));
     }
     private  static boolean areEqual(int expected, int actual){
         return actual==expected;
