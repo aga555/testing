7017df4125dce489257e302738be2f7ed27fec9a
akrawczyk <agammkrawczyk@gmail.com>
2020-11-27 17:57:15 +0100

    testing method with temporary variables

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-        boolean result = 2 + 2 == 4;
-        System.out.println(result);
+       int expected =4;
+       int actual= 2+2;
+       boolean result= areEqual(expected,actual);
+    }
+    private  static boolean areEqual(int expected, int actual){
+        return actual==expected;
     }
 }
