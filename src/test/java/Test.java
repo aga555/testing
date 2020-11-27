a329b52f77251afa17d3f015351b2060ff0a6550
akrawczyk <agammkrawczyk@gmail.com>
2020-11-27 18:34:40 +0100

    cleanup- add more information about tests

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,12 +1,18 @@
 public class Test {
     public static void main(String[] args) {
-    StringCalculator calculator= new StringCalculator();
-    System.out.println(areEqual(2, calculator.add("2")));
-    System.out.println(areEqual(3, calculator.add("2,1")));
-    System.out.println(areEqual(4, calculator.add("2,2")));
+        StringCalculator calculator = new StringCalculator();
+        System.out.println(areEqual(2, calculator.add("2"), "Calculator should return number when number is  given"));
+        System.out.println(areEqual(3, calculator.add("2,1"), "Calculator should return  sum when two number given"));
+        System.out.println(areEqual(4, calculator.add("2,2"), "Calculator should return sum when two number given"));
 
     }
-    private  static boolean areEqual(int expected, int actual){
-        return actual==expected;
+
+    private static String areEqual(int expected, int actual, String s) {
+        if (actual == expected) {
+            return s + " - passed";
+        } else {
+            return s + " - failed";
+        }
+
     }
 }
