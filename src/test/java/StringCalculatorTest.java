b39321508af77a169beb90615059b2d2fede06c6
akrawczyk <agammkrawczyk@gmail.com>
2020-11-27 19:28:32 +0100

    refactor- remove creating instances of clasesas field in test class, create private method

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -3,15 +3,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringCalculatorTest {
-@Test
-    void shouldReturnNumberWhenNumberGiven(){
-    StringCalculator calculator= new StringCalculator();
-    assertEquals(2,calculator.add("2"));
-}
+    StringCalculator calculator;
+
+    @Test
+    void shouldReturnNumberWhenNumberGiven() {
+        createCalculator();
+        assertEquals(2, calculator.add("2"));
+    }
 
     @Test
     void shouldReturnSumWhenTwoNumberGiven() {
-    StringCalculator calculator= new StringCalculator();
-    assertEquals(4,calculator.add("2,2"));
+        createCalculator();
+        assertEquals(4, calculator.add("2,2"));
+    }
+
+    private void createCalculator() {
+        calculator = new StringCalculator();
     }
 }
