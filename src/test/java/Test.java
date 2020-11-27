3180d7613b683e00c9ebb55cf430c0df69588ed2
akrawczyk <agammkrawczyk@gmail.com>
2020-11-27 18:28:09 +0100

    testing  calculator more test cases

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,8 +1,9 @@
 public class Test {
     public static void main(String[] args) {
     StringCalculator calculator= new StringCalculator();
-    int result= calculator.add("2");
-    System.out.println(areEqual(2,result));
+    System.out.println(areEqual(2, calculator.add("2")));
+    System.out.println(areEqual(3, calculator.add("2,1")));
+    System.out.println(areEqual(4, calculator.add("2,2")));
 
     }
     private  static boolean areEqual(int expected, int actual){
