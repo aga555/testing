eb67d11b08459b26ee5fc28d6915024bf70527fd
akrawczyk <agammkrawczyk@gmail.com>
2020-11-28 18:23:03 +0100

    #homework1 #modul1

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,15 +1,15 @@
 public class StringCalculator {
-    public int add(String input){
-        if (input.isEmpty()){
-            return 0;
+
+    public int add(String input) {
+        if (input.isEmpty()) return 0;
+        else {
+            String[] numbers = input.split(",");
+            int  result = 0;
+            for (int i=0; i<numbers.length; i++)
+                result += getIntFrom(numbers[i]);
+            return result;
         }
-        String[] numbers=input.split(",");
-        if(numbers.length==1){
-            String number= numbers[0];
-            return getIntFrom(number);
-        } else {
-            return getIntFrom(numbers[0]) +getIntFrom(numbers[1]);
-        }
+
     }
     private int getIntFrom(String number){
         return Integer.parseInt(number);
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -4,7 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringCalculatorTest {
     StringCalculator calculator;
-
+    @Test
+    void shuldReturnZeroWhenNoNumberGiven() {
+        createCalculator();
+        assertEquals(0,calculator.add(""));
+    }
     @Test
     void shouldReturnNumberWhenNumberGiven() {
         createCalculator();
@@ -15,6 +19,12 @@ class StringCalculatorTest {
     void shouldReturnSumWhenTwoNumberGiven() {
         createCalculator();
         assertEquals(4, calculator.add("2,2"));
+    }
+
+    @Test
+    void shuldREturnSumWhenThreeNumberGiven() {
+        createCalculator();
+        assertEquals(8,calculator.add("5,2,1"));
     }
 
     private void createCalculator() {
