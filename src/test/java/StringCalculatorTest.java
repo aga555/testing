2fce051312ba4d008a96c95d321327b198665630
akrawczyk <agammkrawczyk@gmail.com>
2020-11-29 11:04:57 +0100

    fix spelling mistake after review

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -4,11 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringCalculatorTest {
     StringCalculator calculator;
+
     @Test
-    void shuldReturnZeroWhenNoNumberGiven() {
+    void shouldReturnZeroWhenNoNumberGiven() {
         createCalculator();
-        assertEquals(0,calculator.add(""));
+        assertEquals(0, calculator.add(""));
     }
+
     @Test
     void shouldReturnNumberWhenNumberGiven() {
         createCalculator();
@@ -22,9 +24,9 @@ class StringCalculatorTest {
     }
 
     @Test
-    void shuldREturnSumWhenThreeNumberGiven() {
+    void shouldReturnSumWhenThreeNumberGiven() {
         createCalculator();
-        assertEquals(8,calculator.add("5,2,1"));
+        assertEquals(8, calculator.add("5,2,1"));
     }
 
     private void createCalculator() {
