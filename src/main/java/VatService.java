869858a3dad0b33ee92fa85860f394d0ecea2c63
akrawczyk <agammkrawczyk@gmail.com>
2020-11-29 11:20:58 +0100

    add exception to method getGrossPrice

--- src/main/java/VatService.java
+++ src/main/java/VatService.java
@@ -8,12 +8,15 @@ public class VatService {
         this.vatValue = new BigDecimal("0.23");
     }
 
-    public BigDecimal getGrossPriceForDeafultVat(Product product) {
+    public BigDecimal getGrossPriceForDeafultVat(Product product) throws Exception {
         return getGrossPrice(product.getNetPrice(), vatValue);
     }
 
-    public BigDecimal getGrossPrice(BigDecimal netPrice, BigDecimal vatValue) {
+    public BigDecimal getGrossPrice(BigDecimal netPrice, BigDecimal vatValue) throws Exception {
         MathContext m = new MathContext(4);
+        if (vatValue.compareTo(BigDecimal.ONE) == 1) {
+            throw new Exception("Vat must be lower!");
+        }
         return netPrice.multiply(vatValue.add(BigDecimal.ONE)).round(m);
     }
 }
