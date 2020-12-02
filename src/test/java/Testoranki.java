f19e815afd28b30338581597735cbf47c0285303
akrawczyk <agammkrawczyk@gmail.com>
2020-12-02 16:14:01 +0100

    #1Testoranki

--- src/test/java/Testoranki.java
+++ src/test/java/Testoranki.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Testoranki {
     @Test
-    void shouldReturnSumWhenTwoNumberAddTwo() {
+    void shouldReturnSumWhenTwoNumberGiven() {
         int expected = 2 + 2;
         Assertions.assertEquals(4, expected);
     }
@@ -19,8 +21,17 @@ public class Testoranki {
         boolean expected = actual.contains(letter);
         //then
         assertTrue(expected);
-
     }
 
+    @Test
+    void shouldReturnWhenGivenDateIsFromPast() {
+        //given
+        Date d1 = new Date(2220, 11, 25, 8, 30);
+        Date d2 = new Date(System.currentTimeMillis());
+        //when
+        boolean isFromPast = d1.compareTo(d2) < 0;
+        //then
+        assertTrue(isFromPast);
+    }
 }
 
