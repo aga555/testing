0d8b56f750660edaf2241ef851d03b5800cde001
akrawczyk <agammkrawczyk@gmail.com>
2020-12-04 15:28:45 +0100

    #homework modul2- add assertJ

--- homework/src/main/java/SimpleProduct.java
+++ homework/src/main/java/SimpleProduct.java
@@ -1,4 +1,3 @@
-
 public class SimpleProduct {
     String id;
     double netPrice;
--- homework/src/test/java/SimpleVatServiceTest.java
+++ homework/src/test/java/SimpleVatServiceTest.java
@@ -1,30 +1,30 @@
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.StrictAssertions.assertThatThrownBy;
 
 class SimpleVatServiceTest {
+    double vatValue = 0.23;
+    SimpleVatService simpleVatService;
 
     @Test
     void getGrossPriceForDefaultVat() throws Exception {
         //given
         SimpleProduct simpleProduct = new SimpleProduct("1", 10.00);
-        SimpleVatService simpleVatService = new SimpleVatService(0.23);
         //when
         double result = simpleVatService.getGrossPriceForDefaultVat(simpleProduct);
         //then
-        Assertions.assertEquals(12.30, result);
+        assertThat(result).isEqualTo(12.30);
     }
+
 
     @Test
     void getGrossPrice() throws Exception {
-        //given
-        double vat = 0.23;
-        SimpleVatService simpleVatService = new SimpleVatService(vat);
         //when
-        double result = simpleVatService.getGrossPrice(100, vat);
+        double result = simpleVatService.getGrossPrice(100, 0.23);
         //then
-        Assertions.assertEquals(123.00, result);
+        assertThat(result).isEqualTo(123);
 
     }
 
@@ -32,12 +32,15 @@ class SimpleVatServiceTest {
     void shouldThrowExceptionWhenVatIsTooHigh() throws Exception {
         double vatValue = 1.23;
         SimpleProduct simpleProduct = new SimpleProduct("1", 100.00);
-        SimpleVatService simpleVatService = new SimpleVatService(vatValue);
         assertThatThrownBy(() -> {
             simpleVatService.getGrossPrice(simpleProduct.getNetPrice(), vatValue);
         }).isInstanceOf(Exception.class)
                 .hasMessageContaining("Vat must be lower!");
+    }
 
+    @BeforeEach
+    void prepareVatService() {
+        simpleVatService = new SimpleVatService(vatValue);
     }
 
 }
