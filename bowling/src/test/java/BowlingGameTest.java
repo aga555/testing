bc8861027ac5619fbdc10a88c1f427afcfef3baf
akrawczyk <agammkrawczyk@gmail.com>
2020-12-05 15:15:25 +0100

    remove test with Junit

--- bowling/src/test/java/BowlingGameTest.java
+++ bowling/src/test/java/BowlingGameTest.java
@@ -1,16 +1,9 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.StrictAssertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BowlingGameTest {
     BowlingGame theGame = new BowlingGame();
-
-    @Test
-    void shouldScoreWhenRolledOneWithJunit() {
-        assertEquals(1, theGame.getScore());
-
-    }
 
     @Test
     void shouldScoreWhenRolledOneWithAssertJ() {
